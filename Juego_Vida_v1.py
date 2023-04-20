e009f3133aaccc2bbb68a69d5ce6b9bb4786bdc5
Damian Pinedo Moreno <118433455+DPM-ES@users.noreply.github.com>
2023-04-20 17:56:09 +0200

    Corregido dos errores y añadido controles de tiempo

--- Juego_Vida_v1.py
+++ Juego_Vida_v1.py
@@ -1,13 +1,44 @@
 """
     El juego de la vida
-    Version: 1.0
+    Version: 1.1
     Proyecto realizado en Python usando la librería pygame
     Autor: DPM_ES
 """
-
 # Importaciones
 import pygame
+import sys
 import numpy as np
+
+# Función que controla qué se pulsa por tecládo y las funciones a realizar
+def control_key(key, pause, new_game_state, i_timer):
+    # Pausa el juego a pulsar la barra espaciadora
+    if key == pygame.K_SPACE:
+        pause = not pause
+
+    # Al pulsar f, pone todas las casillas a 1 (vivas)
+    if key == pygame.K_f:
+        new_game_state = np.full((num_cel_x, num_cel_y), 1)
+
+    # Al pulsar e, pone todas las casillas a 0 (muertas)
+    if key == pygame.K_e:
+        new_game_state = np.zeros((num_cel_x, num_cel_y))
+
+    # Al pulsar q, cierra la ventana, igual que al cerrar pulsando en la X de cerrar
+    if key == pygame.K_q:
+        pygame.quit()
+        sys.exit()
+    
+    # Hace que el juego vaya más despacio
+    if key == pygame.K_KP_MINUS:
+        if i_timer < 4:
+            i_timer += 1
+    
+    # Hace que el juego vaya más deprisa
+    if key == pygame.K_KP_PLUS:
+        if i_timer > 0:
+            i_timer -= 1
+
+    return pause, new_game_state, i_timer
 
 # Inicialización de los módulos de pygame
 pygame.init()
@@ -34,8 +65,11 @@ dim_cel_w = width / num_cel_x
 # Estado de las celdas juego. 1: Vivas; 0: Muertas
 game_state = np.zeros((num_cel_x, num_cel_y))
 
-# Estado de actividad del juego
+# Controladores de estado del juego
 pause = True
+timer = [50, 100, 200, 300, 400]
+i_timer = 2
+
 
 # Bucle de ejecución
 while True:
@@ -45,7 +79,7 @@ while True:
 
     # Reseteamos los colores de la pantalla para poder representar el estado sin solapamientos y
     game_field.fill(c_blak)
-    pygame.time.wait(100)
+    pygame.time.wait(timer[i_timer])
 
     # Recogemos eventos de teclado y ratón
     for event in pygame.event.get():
@@ -53,25 +87,16 @@ while True:
         # Evento de plusar en cerrar ventana y acción de cerrar juego
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
 
-        # Evento de pulsar el teclado
+        # Evento de pulsar el teclado, llama la función de control
         if event.type == pygame.KEYDOWN:
-            # Pausa el juego a pulsar la barra espaciadora
-            if event.key == pygame.K_SPACE:
-                pause = not pause
-            # Al pulsar f, pone todas las casillas a 1 (vivas)
-            if event.key == pygame.K_f:
-                new_game_state = np.full((num_cel_x, num_cel_y), 1)
-
-            # Al pulsar e, pone todas las casillas a 0 (muertas)
-            if event.key == pygame.K_e:
-                new_game_state = np.zeros((num_cel_x, num_cel_y))
+            pause, new_game_state, i_timer = control_key(event.key, pause, new_game_state, i_timer)
 
         # Recoge el click izquierdo y pone la casilla en 1
         if pygame.mouse.get_pressed()[0]:
             pos_x, pos_y = pygame.mouse.get_pos()
             cel_x, cel_y = int(np.floor(pos_x / dim_cel_w)), int(np.floor(pos_y / dim_cel_h))
-            # new_game_state[cel_x, cel_y] = 1
             try:
                 new_game_state[cel_x, cel_y] = 1
             except IndexError:
