7ec21dfa5090674cd230c511a4609ee22181f016
Damian Pinedo Moreno <118433455+DPM-ES@users.noreply.github.com>
2023-04-23 11:29:15 +0200

    Add files via upload

--- Juego_Vida_v1_color.py
+++ Juego_Vida_v1_color.py
@@ -1,6 +1,6 @@
 """
-    El juego de la vida
-    Version: 1.2
+    El juego de la vida (con color)
+    Version: 1.0
     Proyecto realizado en Python usando la librería pygame
     Autor: DPM_ES
 """
@@ -12,18 +12,18 @@ import numpy as np
 
 # Función que controla qué se pulsa por tecládo y las funciones a realizar
 def control_key(key):
-    global pause, i_timer, new_game_state
+    global pause, i_timer, new_game_state, i_color
     # Pausa el juego a pulsar la barra espaciadora
     if key == pygame.K_SPACE:
         pause = not pause
 
     # Al pulsar f, pone todas las casillas a 1 (vivas)
     if key == pygame.K_f:
-        new_game_state = np.full((num_cel_x, num_cel_y), 1)
+        new_game_state = np.full((num_cel_x, num_cel_y, 3), 1)
 
     # Al pulsar e, pone todas las casillas a 0 (muertas)
     if key == pygame.K_e:
-        new_game_state = np.zeros((num_cel_x, num_cel_y))
+        new_game_state = np.zeros((num_cel_x, num_cel_y, 3))
 
     # Al pulsar q, cierra la ventana, igual que al cerrar pulsando en la X de cerrar
     if key == pygame.K_q:
@@ -40,20 +40,40 @@ def control_key(key):
         if i_timer > 0:
             i_timer -= 1
 
+    # Activa el color rojo
+    if key == pygame.K_1:
+        i_color = 0
+
+    # Activa el color verde
+    if key == pygame.K_2:
+        i_color = 1
+
+    # Activa el color azul
+    if key == pygame.K_3:
+        i_color = 2
+
+    # Activa el color azul
+    if key == pygame.K_4:
+        i_color = 3
+
+
+# Función para generar los colores
+def color_maker(status):
+    return 255 * status[0], 255 * status[1], 255 * status[2]
+
 
 # Inicialización de los módulos de pygame
 pygame.init()
 
 # Colores
 c_blak = (0, 0, 0)
-c_green = (0, 255, 0)
-c_ggray = (100, 200, 100)
+c_gray = (128, 128, 128)
 c_red = (255, 0, 0)
 
 # Tamño e inicialización de la pantalla del juego
 height = width = 1000  # Nota: Se puede cambiar este valor para hacer la pantalla más o menos grande
 game_field = pygame.display.set_mode((width + 1, height + 1))  # Nota: añado un pixel para que no se corte la cuadrícula
-pygame.display.set_caption("DPM: El juego de la vida (v1.2)")
+pygame.display.set_caption("DPM: El juego de la vida a color (v1.0)")
 game_field.fill(c_blak)
 external_box = [(0, 0), (width, 0), (width, height), (0, height)]
 
@@ -64,12 +84,13 @@ dim_cel_w = width / num_cel_x
 
 
 # Estado de las celdas juego. 1: Vivas; 0: Muertas
-game_state = np.zeros((num_cel_x, num_cel_y))
+game_state = np.zeros((num_cel_x, num_cel_y, 3))
 
 # Controladores de estado del juego
 pause = True
 timer = [50, 100, 200, 300, 400]
 i_timer = 2
+i_color = 0  # 0: Rojo; 1: Verde; 2: Azul; 3: Blanco
 
 
 # Bucle de ejecución
@@ -99,7 +120,10 @@ while True:
             pos_x, pos_y = pygame.mouse.get_pos()
             cel_x, cel_y = int(np.floor(pos_x / dim_cel_w)), int(np.floor(pos_y / dim_cel_h))
             try:
-                new_game_state[cel_x, cel_y] = 1
+                if i_color == 3:
+                    new_game_state[cel_x, cel_y] = 1
+                else:
+                    new_game_state[cel_x, cel_y][i_color] = 1
             except IndexError:
                 pass
 
@@ -130,14 +154,14 @@ while True:
                           game_state[(x + 1) % num_cel_x, (y + 1) % num_cel_y]
 
                 #Reglas del juego de la vida
+                for color in range(3):
+                    # Regla 1: Si una célula está viva y tiene dos o tres vecinas vivas, sobrevive.
+                    if game_state[x, y][color] == 1 and (n_neigh[color] < 2 or n_neigh[color] > 3):
+                        new_game_state[x, y][color] = 0
 
-                # Regla 1: Si una célula está viva y tiene dos o tres vecinas vivas, sobrevive.
-                if game_state[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
-                    new_game_state[x, y] = 0
-
-                # Regla 2: Si una célula está muerta y tiene tres vecinas vivas, nace.
-                elif game_state[x, y] == 0 and n_neigh == 3:
-                    new_game_state[x, y] = 1
+                    # Regla 2: Si una célula está muerta y tiene tres vecinas vivas, nace.
+                    elif game_state[x, y][color] == 0 and n_neigh[color] == 3:
+                        new_game_state[x, y][color] = 1
 
             # Representación de las cajas que contienen las celdas
             # Se genera dibujando un poligono siguiendo las coordenadas de cada vertice.
@@ -147,9 +171,9 @@ while True:
                         ( x      * dim_cel_w, (y + 1) * dim_cel_h)]
 
             # Dibujamos la celda
-            if new_game_state[x, y]:
-                pygame.draw.polygon(game_field, c_green, cell_box, 0)
-            pygame.draw.polygon(game_field, c_ggray, cell_box, 1)
+            if new_game_state[x, y].any():
+                pygame.draw.polygon(game_field, color_maker(new_game_state[x, y]), cell_box, 0)
+            pygame.draw.polygon(game_field, c_gray, cell_box, 1)
 
     # Dibujamos en rojo el contorno cuando el juego está en pausa
     if pause:
